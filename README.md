38d898a54d89dd43d8e8fcd292a160a528609027
HaseebBhai-PC\Haseeb Bhai <haseeb_khan33@hotmail.co.uk>
2017-03-01 11:09:25 +0500

    Creating a map with a marker location

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Leaflet Webmaps with Python and Folium
 The interactive web map with marker list of cordinaties provides
 
-<coe>pip install folium</code>
+<code>pip install folium</code>
+
+if not installed and through an error "you don't have jinjar2" then
+
+<code>pip install jinjar2</code>
+
+<code>pip install folium</code>
--- script.py
+++ script.py
@@ -1,10 +1,13 @@
 import folium
 
-map = folium.Map(location = [45.372, -121.697], zoom_start = 3, tiles = "Stamen Terrain")
+map = folium.Map(location = [31.471132, 74.260050], zoom_start = 3, tiles = "Stamen Terrain")
 # .Map() creates a folium map object 
 # location is cordinates
 # zoom_start is zoom level min is 1
 # tiles shows the map style
+
+map.simple_marker(location = [31.469935, 74.260096], popup = "Zam Zam Tower", marker_color = "red", popup_width = 500)
+#Adding marker for a location
 
 map.create_map(path = "test.html")
 
