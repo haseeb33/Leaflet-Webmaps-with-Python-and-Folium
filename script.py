9b2bbf7b2fb2a72b34a03a0033e2e2dce936fa8f
HaseebBhai-PC\Haseeb Bhai <haseeb_khan33@hotmail.co.uk>
2017-03-01 12:18:25 +0500

    Conditional markers and dynamic center point

--- script.py
+++ script.py
@@ -4,15 +4,26 @@ import pandas
 df = pandas.read_csv("Volcanoes-USA.txt")
 # Reading all the valcanoes info and saving it a df
 
-map = folium.Map(location = [42.879902, -113.221001], zoom_start = 2.5, tiles = "Stamen Terrain")
+map = folium.Map(location = [df["LAT"].mean(), df["LON"].mean()], zoom_start = 4, tiles = "Stamen Terrain")
 # .Map() creates a folium map object 
 # location is cordinates
 # zoom_start is zoom level min is 1
 # tiles shows the map style
 
-for lat, lon, name in zip(df["LAT"], df["LON"], df["NAME"]):
+for lat, lon, name, elev in zip(df["LAT"], df["LON"], df["NAME"], df["ELEV"]):
     #Adding all the markers
-    map.simple_marker(location = [lat, lon], popup = name, marker_color = "red")
+    color = "blue"
+    mini = int(min(df["ELEV"]))
+    step = int((max(df["ELEV"]) - min(df["ELEV"]))/3)
+    
+    if elev in range(mini, mini + step):
+        color = "green"
+    elif elev in range(mini + step, mini + step*2):
+        color = "orange"
+    else:
+        color = "red"
+        
+    map.simple_marker(location = [lat, lon], popup = name, marker_color = color)
 
 
 map.create_map(path = "test.html")
