98f7cbefd9a762f88bbde4f28ceee54da136a109
HaseebBhai-PC\Haseeb Bhai <haseeb_khan33@hotmail.co.uk>
2017-03-01 14:14:37 +0500

    removed depricated methods and Adding a Choropleth Map from GeoJson

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import pandas
 df = pandas.read_csv("Volcanoes-USA.txt")
 # Reading all the valcanoes info and saving it a df
 
-map = folium.Map(location = [df["LAT"].mean(), df["LON"].mean()], zoom_start = 4, tiles = "Stamen Terrain")
+map = folium.Map(location = [df["LAT"].mean(), df["LON"].mean()], zoom_start = 4, tiles = "Mapbox bright")
 # .Map() creates a folium map object 
 # location is cordinates
 # zoom_start is zoom level min is 1
@@ -23,8 +23,12 @@ for lat, lon, name, elev in zip(df["LAT"], df["LON"], df["NAME"], df["ELEV"]):
     else:
         color = "red"
         
-    map.simple_marker(location = [lat, lon], popup = name, marker_color = color)
+    map.add_child(folium.Marker(location = [lat, lon], popup = name, icon = folium.Icon(color = color)))
+    
+map.add_child(folium.GeoJson(data = open("World-population.geojson"),
+     name = "World Population",
+     style_function = lambda x: {"fillColor": "green" if x["properties"]["POP2005"] <= 10000000 else "orange" if 10000000 < x["properties"]["POP2005"] < 20000000 else "red"}))
+#Adding a Choropleth Map from GeoJson
 
-
-map.create_map(path = "test.html")
+map.save(outfile = "test.html")
 # .create_map() create a html file from map object
