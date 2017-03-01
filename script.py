656f195c22d26eeaa506b1417c5ae3d8efbedb11
HaseebBhai-PC\Haseeb Bhai <haseeb_khan33@hotmail.co.uk>
2017-03-01 11:37:03 +0500

    markers added on respective location from txt file

--- script.py
+++ script.py
@@ -1,14 +1,19 @@
 import folium
+import pandas
 
-map = folium.Map(location = [31.471132, 74.260050], zoom_start = 3, tiles = "Stamen Terrain")
+df = pandas.read_csv("Volcanoes-USA.txt")
+# Reading all the valcanoes info and saving it a df
+
+map = folium.Map(location = [42.879902, -113.221001], zoom_start = 2.5, tiles = "Stamen Terrain")
 # .Map() creates a folium map object 
 # location is cordinates
 # zoom_start is zoom level min is 1
 # tiles shows the map style
 
-map.simple_marker(location = [31.469935, 74.260096], popup = "Zam Zam Tower", marker_color = "red", popup_width = 500)
-#Adding marker for a location
+for lat, lon, name in zip(df["LAT"], df["LON"], df["NAME"]):
+    #Adding all the markers
+    map.simple_marker(location = [lat, lon], popup = name, marker_color = "red")
+
 
 map.create_map(path = "test.html")
-
 # .create_map() create a html file from map object
